67cdaefa0b5509d9818ceac46051b13d0540a98f
cross0707 <14088@hakodate.kosen-ac.jp>
2018-12-20 11:36:58 +0900

    ジャンプ,ネギ追加

---
+++ gu.java
@@ -0,0 +1,16 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class gu here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class gu extends Actor
+{
+    /**
+     * Act - do whatever the gu wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    
+}
--- kinoko.java
+++ kinoko.java
@@ -11,28 +11,90 @@ public class kinoko extends Actor
     /**
      * Act - do whatever the kinoko wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+     */         
+   private int ySpeed;
+   private int apexTimer;
+   private int get;
+    public kinoko()
+    {
+    }
+    
     public void act() 
     {
-        if( Greenfoot.isKeyDown( "down" ))
+        move();
+        get();
+        scrole();
+    }    
+    public void move(){
+        int groundLevel = getWorld().getHeight() - getImage().getHeight()/2;
+        boolean onGround = (getY() == groundLevel);
+        if (!onGround) // in middle of jump
         {
-            setRotation(90);
-            move(4);
-        }// Add your action code here.
-        if(Greenfoot.isKeyDown("left"))// Add your action code here.
-        {
-            setRotation(180);
-            move(4);       
+            if (ySpeed == 0 && apexTimer > 0) apexTimer--; // run apex timer
+            if (ySpeed == 0 && apexTimer > 0) return; // apex timer still running
+            ySpeed++; // adds gravity effect
+            setLocation(getX(), getY()+ySpeed); // fall (rising slower or falling faster)
+            if (getY()>=groundLevel) // has landed (reached ground level)
+            {
+                setLocation(getX(), groundLevel); // set on ground
+                Greenfoot.getKey(); // clears any key pressed during jump
+           }
         }
-        if( Greenfoot.isKeyDown( "up" ))
+        else // on ground
+        {
+            if ("space".equals(Greenfoot.getKey())) // jump key detected
+            {
+                ySpeed = -13; // add jump speed
+                setLocation(getX(), getY()+ySpeed); // leave ground
+                apexTimer = 4;  // set apex timer (adjust value to suit)
+            }
+        }
+        
+        /*if( Greenfoot.isKeyDown( "up" ))
         {
             setRotation(270);
             move(4);
         }
+        if( Greenfoot.isKeyDown( "down" ))
+        {
+            setRotation(90);
+            move(4);
+        }// Add your action code here.*/
+        if(Greenfoot.isKeyDown("left"))// Add your action code here.
+        {
+            setRotation(360);
+            move(-4);       
+        }
+        
         if(Greenfoot.isKeyDown("right"))// Add your action code here.
         {
             setRotation(360);
             move(4);       
         }
-    }    
+    }
+    public void get(){
+        Actor actor = getOneObjectAtOffset( 0, 0, gu.class ); 
+        if( actor != null ){ 
+
+           get++;
+           getWorld().removeObject( actor ); 
+           if(get >= 10) { 
+
+               getWorld().showText("　　ゲームクリア！", 50, 50); 
+
+               Greenfoot.stop(); 
+
+            }else { 
+
+                getWorld().showText("" +get, 50, 50); 
+
+            } 
+    }
 }
+    public void scrole(){
+        
+        
+        move(-2);
+    }
+    }
+
