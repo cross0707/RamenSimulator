38a369fbf0c0bb101eea5904fe96ad084b797a12
a-a-a-a-a-a-a-a-a-a <14133@hakodate.kosen-ac.jp>
2019-01-10 10:07:51 +0900

    箸の画像設定

---
+++ hashi.java
@@ -0,0 +1,23 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class hashi here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class hashi extends gu
+{
+    /**
+     * Act - do whatever the hashi wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+    public void scrole(){
+        move(-2);
+    }
+}
+
