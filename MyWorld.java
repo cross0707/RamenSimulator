f3691981f5b13d1eccbb24c69ab730bedbb4cf0f
a-a-a-a-a-a-a-a-a-a <14133@hakodate.kosen-ac.jp>
2019-01-31 10:16:26 +0900

    背景スクロール

--- MyWorld.java
+++ MyWorld.java
@@ -9,6 +9,16 @@ import greenfoot.*;
 public class MyWorld extends World
 {
     private int rtime;
+    
+         
+    private static final String bgImageName = "2443999i.jpeg";
+    private static final double scrollSpeed = 2.0;
+    private static final int picWidth = (new GreenfootImage(bgImageName)).getWidth();
+ 
+    private GreenfootImage bgImage, bgBase;
+    private int scrollPosition = 0;
+     
+    
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -16,7 +26,11 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(620, 439, 1); 
+        super(620, 439, 1);
+        setBackground(bgImageName);
+        bgImage = new GreenfootImage(getBackground());
+        bgBase = new GreenfootImage(picWidth, getHeight());
+        bgBase.drawImage(bgImage, 0, 0);
         addObject( new kinoko(), 75, 410 );
         
         int X1=150,X2=620,Y1=0,Y2=439; 
@@ -41,6 +55,11 @@ public class MyWorld extends World
     
      }
      public void act(){
+         
+         scrollPosition -= scrollSpeed;
+        while(scrollSpeed > 0 && scrollPosition < -picWidth) scrollPosition += picWidth;
+        while(scrollSpeed < 0 && scrollPosition > 0) scrollPosition -= picWidth;
+        paint(scrollPosition);
        int X1=520,X2=620,Y1=0,Y2=439; 
 
         int x,y; 
@@ -69,4 +88,10 @@ public class MyWorld extends World
         static int scr ;
     static public int getscr(){ return scr;}
     
+         private void paint(int position)
+    {
+        GreenfootImage bg = getBackground();
+        bg.drawImage(bgBase, position, 0);
+        bg.drawImage(bgImage, position + picWidth, 0);
+    }
 }
